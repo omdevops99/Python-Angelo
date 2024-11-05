e04c4f213254f99f921eed56e5c6e2f479f7f7ce
om <omejjigiri99@gmail.com>
2024-11-05 15:49:09 +0530

    c

---
+++ Random_module.py
@@ -0,0 +1,18 @@
+import random
+import My_module
+#it is a python module,
+# How we generate the random integer between a & b
+#what exactly is module -> Split the code into the multiple modules Ex: building the CAR everyone wil not work on whole car. Each will work on the single tyre module,engine modulw Etc.
+# How to create own python module
+# create a file name my_module.py 
+
+# random_integer = random.randint (1,10 )
+
+random_number_0_to_1 = random.random() * 10
+
+print(random_number_0_to_1)
+
+
+# print(random_integer)
+# print(My_module.my_favourite_number)
+
